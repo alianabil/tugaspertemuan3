e147cfdb0ee59e4ce6cfe97aa89c7c43d230e2e3
alianabil <alianabila3772@gmail.com>
2025-04-23 23:15:41 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Counter from "./Components/Counter";
 import Navbar from "./Components/Navbar";
+import Productcard from "./Components/Productcard";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,16 @@ function App() {
     <div>
       <Navbar title="TECHMART" />
       <Counter />
+      <Productcard
+        namaproduk="smartphone"
+        harga="Rp. 1.000.000"
+        image="https://cdn.timesmedia.co.id/images/2021/12/29/smartphone-2022.jpg"
+      />
+      <Productcard
+        namaproduk="laptop"
+        harga="Rp 2.000.000"
+        image="https://cdn.pixabay.com/photo/2020/10/21/18/07/laptop-5673901_1280.jpg"
+      />
     </div>
   );
 }
